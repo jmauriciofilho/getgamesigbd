09f28bec011634001e624a12f922dddc8ad06f87
Mauricio Pereira <josemauriciobpfilho@gmail.com>
2022-08-13 14:14:44 -0300

    adicionando array de slugs para salvar game

--- index.js
+++ index.js
@@ -1,101 +1,132 @@
 const axios = require('axios')
 const {Client} = require('@notionhq/client')
+const jsonData = require('./games.json')
 
-// ----- credenciais API IGBD -------------
-const client_id = "t2ia0m1vobq1z03mj5fi15xo4kkqgl"
-const client_secret = "ubjqijinyyxf279hrukk14rtliyzjd"
-const grant_type = "client_credentials"
-let access_token = ""
-let token_type = ""
-// ----------------------------------------
+const httpRequest = async (url, method, headers, data) => {
+    const response = await axios({
+        url: url,
+        method: method,
+        headers: headers,
+        data: data
+    })
 
-// ----- credenciais API notion -----------
-const notion_key = "secret_8Pkgoyeu2NKu19ozxkS4NiCkRFlUSVXHDxtlbx0fUqb"
-const notion_data_base_id = "87d35877b1e448d7827d903de1c19413"
-const notion = new Client({
-    auth: notion_key
-})
-// ----------------------------------------
+    return response
+}
 
-// TODO: aceitar um array de slugs de um arquivo json
-const slug = "teenage-mutant-ninja-turtles-shredders-revenge"
-let cover = ""
-let game = {}
+const authAPiIgdb = async (url, method, headers, data) => {
+    const authResponse = await httpRequest(url, method, headers, data)
+    const authCredentials = {
+        'access_token': authResponse.data['access_token'],
+        'token_type': authResponse.data['token_type']
+    }
+    return authCredentials
+}
 
-axios({
-    method: "post",
-    url: `https://id.twitch.tv/oauth2/token?client_id=${client_id}&client_secret=${client_secret}&grant_type=${grant_type}`
-}).then(function(response) {
+const getGameBySlug = async (url, method, headers, data) => {
+    const responseGame = await httpRequest(url, method, headers, data)
+    const game = {
+        'name': responseGame.data[0]['name'],
+        'coverId': responseGame.data[0]['cover']
+    }
+    return game
+}
 
-    access_token = response.data['access_token']
-    token_type = response.data['token_type']
-    axios({
-        method: "post",
-        url: "https://api.igdb.com/v4/games",
-        headers: {
-            'Accept': 'application/json',
-            'Client-ID': client_id,
-            'Authorization': `Bearer ${access_token}`
+const getCoverById = async (url, method, headers, data) => {
+    const responseGame = await httpRequest(url, method, headers, data)
+    const image_id = responseGame.data[0]['image_id']
+    const cover = `https://images.igdb.com/igdb/image/upload/t_original/${image_id}.jpg`
+    return cover
+}
+
+const savePageNotion = async (notion, notion_data_base_id, gameCover, gameName) => {
+    notion.pages.create({
+        "parent": {
+            "database_id": notion_data_base_id
         },
-        data: `fields *; where slug = "${slug}";`
-    }).then( response => {
-        cover = response.data[0]['cover']
-        game.name = response.data[0]['name']
-
-        axios({
-            url: "https://api.igdb.com/v4/covers",
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Client-ID': client_id,
-                'Authorization': `Bearer ${access_token}`
-            },
-            data: `fields *; where id = ${cover};`
-        }).then(response => {
-            const image_id = response.data[0]['image_id']
-            game.cover = `https://images.igdb.com/igdb/image/upload/t_original/${image_id}.jpg`
-
-            notion.pages.create({
-                "parent": {
-                    "database_id": notion_data_base_id
-                },
-                "cover": {
-                    "type": "external",
-                    "external": {
-                        "url": game.cover
-                    }
-                },
-                "icon": {
-                    "type": "emoji",
-                    "emoji": "🎮"
-                },
-                "properties": {
-                    "Nome": { 
-                      "title":[
-                        {
-                          "text": {
-                            "content": game.name
-                          }
-                        }
-                      ]
-                    },
-                    "Status": {
-                        "select": {
-                            "name": "Vazio"
-                        }
-                    }
+        "cover": {
+            "type": "external",
+            "external": {
+                "url": gameCover
+            }
+        },
+        "icon": {
+            "type": "emoji",
+            "emoji": "🎮"
+        },
+        "properties": {
+            "Nome": { 
+            "title":[
+                {
+                "text": {
+                    "content": gameName
                 }
-            }).then(() => {
-                console.log('Game salvo com sucesso!')
-                console.log(`Nome: ${game.name}`)
-            })
+                }
+            ]
+            },
+            "Status": {
+                "select": {
+                    "name": "Vazio"
+                }
+            }
+        }
+    })
+}
 
-        }).catch(err => {
-            console.error(err);
-        });
-    }).catch(err => {
-        console.error(err);
-    });
-}).catch(err => {
-    console.error(err);
-});
+const main = async () => {
+    // ----- credenciais API IGBD -------------
+    const client_id = "t2ia0m1vobq1z03mj5fi15xo4kkqgl"
+    const client_secret = "ubjqijinyyxf279hrukk14rtliyzjd"
+    const grant_type = "client_credentials"
+    // ----------------------------------------
+
+    // ----- credenciais API notion -----------
+    const notion_key = "secret_8Pkgoyeu2NKu19ozxkS4NiCkRFlUSVXHDxtlbx0fUqb"
+    const notion_data_base_id = "87d35877b1e448d7827d903de1c19413"
+    const notion = new Client({
+        auth: notion_key
+    })
+    // ----------------------------------------
+
+    try{
+        const authCredentials = await authAPiIgdb(
+            `https://id.twitch.tv/oauth2/token?client_id=${client_id}&client_secret=${client_secret}&grant_type=${grant_type}`, 
+            "post", 
+            {}, 
+            ""
+        )
+    
+        Object.keys(jsonData).forEach(async function(key, value){
+            let slug = jsonData[key]
+            const game = await getGameBySlug(
+                "https://api.igdb.com/v4/games", 
+                "post", 
+                {
+                    'Accept': 'application/json',
+                    'Client-ID': client_id,
+                    'Authorization': `Bearer ${authCredentials['access_token']}`
+                },
+                `fields *; where slug = "${slug}";`
+            )
+        
+            const cover = await getCoverById(
+                "https://api.igdb.com/v4/covers",
+                "post",
+                {
+                    'Accept': 'application/json',
+                    'Client-ID': client_id,
+                    'Authorization': `Bearer ${authCredentials['access_token']}`
+                },
+                `fields *; where id = ${game["coverId"]};`
+            )
+        
+            await savePageNotion(notion, notion_data_base_id, cover, game["name"])
+    
+            console.log(`Game salvo: ${game.name}`)
+        })
+    }catch(err){
+        console.log(err)
+    }
+    
+}
+
+main()
